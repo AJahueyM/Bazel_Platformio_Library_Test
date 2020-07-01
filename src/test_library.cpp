416159db3b1a15a75e919b4ccaff120e88ec125d
Alberto Jahuey Moncada <albertojahueymoncada@gmail.com>
2020-07-01 11:36:09 -0500

    Test class

---
+++ src/test_library.cpp
@@ -0,0 +1,15 @@
+#include "test_library.h"
+
+Test::Test(){
+
+}
+
+int Test::sum(int a, int b){
+	int res = a + b;
+	sumation += res;
+	return res;
+}
+
+int Test::getSumation(){
+	return sumation;
+}
---
+++ src/test_library.h
@@ -0,0 +1,14 @@
+
+#ifndef TEST__HEADER
+#define TEST__HEADER
+
+class Test {
+public:
+        Test();
+        int sum(int a, int b);
+        int getSumation();
+private:
+        int sumation = 0;
+};
+
+#endif
